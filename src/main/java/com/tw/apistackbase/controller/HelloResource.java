bc89e8ef3d52ab731019d9e077bef5fc25576cb6
LiDongpeng1 <1609640094@qq.com>
2019-07-16 18:13:45 +0800

    [lidongpeng]after class

--- src/main/java/com/tw/apistackbase/controller/HelloResource.java
+++ src/main/java/com/tw/apistackbase/controller/HelloResource.java
@@ -1,26 +1,66 @@
 package com.tw.apistackbase.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
  * Created by jxzhong on 18/08/2017.
  */
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/employees")
 public class HelloResource {
+
+    List<Employee> employees = new ArrayList();
 
     private final Logger log = Logger.getLogger(this.getClass().getName());
 
-    @GetMapping(path = "/{userName}", produces = {"application/json"})
-    public ResponseEntity<String> getAll(@PathVariable String userName) {
+//    @GetMapping(path = "/{userName}", produces = {"application/json"})
+//    public ResponseEntity<String> getAll(@PathVariable String userName) {
+//
+//        return ResponseEntity.ok("Hello:" + userName);
+//    }
 
-        return ResponseEntity.ok("Hello:" + userName);
+//    @GetMapping(path = "/{userName}", produces = {"application/json"})
+    @GetMapping()
+    public ResponseEntity getAll() {
+        employees.add(new Employee(0,"zhangsan",20,"male"));
+        return ResponseEntity.ok().body(employees);
+    }
+
+    @PostMapping()
+    public ResponseEntity addEmployee(@RequestBody Employee employee) {
+        employees.add(employee);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+//        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping()
+    public ResponseEntity changeEmployee(@RequestBody Employee employee,@RequestParam int id) {
+        employees.forEach(item -> {
+            if(item.getId() == employee.getId()){
+                item.setAge(employee.getAge());
+                item.setGender(employee.getGender());
+                item.setName(employee.getName());
+            }
+                }
+                );
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping()
+    public ResponseEntity deleteEmployee(@RequestParam int id) {
+        employees.forEach(item -> {
+                    if(item.getId() == id){
+                        employees.remove(item);
+                    }
+                }
+        );
+        return ResponseEntity.ok().build();
     }
 
 
--- src/test/java/com/tw/apistackbase/ApiStackBaseApplicationTests.java
+++ src/test/java/com/tw/apistackbase/ApiStackBaseApplicationTests.java
@@ -1,17 +1,35 @@
 package com.tw.apistackbase;
 
 import org.junit.Test;
+import static org.hamcrest.Matchers.containsString;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.engine.TestExecutionResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@AutoConfigureMockMvc
+
 public class ApiStackBaseApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+
 	@Test
-	public void contextLoads() {
-		
+	public void contextLoads() throws Exception{
+		this.mockMvc.perform(get("/employees")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("helloworld")));
+
 	}
 
 }
